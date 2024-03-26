6277951d2042b37a94136b4fbc1302c1f8a7770e
Gabriel Greghi <141462152+dreaMykj@users.noreply.github.com>
2024-03-25 22:23:17 -0300

    Aula do dia 25/03
    Cards

---
+++ listar_users.php
@@ -0,0 +1,34 @@
+<?php 
+
+//Dados para conexão do banco
+$servidor = "localhost";
+$user = "root";
+$senha = "root";
+$banco = "bd_sistema";
+
+//conectando no banco de dados utilizando a função mysqli
+$conexao = new mysqli($servidor, $user, $senha, $banco);
+
+// dados para seleção de alunos
+
+$select = "select * from aluno";
+
+// executar o comando select dentro do banco utilizando a função mysqli query
+
+$query = mysqli_query($conexao, $select);
+
+//pegar o primeiro registro do banco
+
+while($array = mysqli_fetch_array($query))
+{
+    echo $array ['rm_aluno'];
+    echo "<br>";
+    echo $array ['nm_aluno'];
+    echo "<br>";
+    echo $array ['email'];
+    echo "<br>";
+    echo $array ['celular'];
+    echo "<br>";
+    print_r($array);
+}
+?>
